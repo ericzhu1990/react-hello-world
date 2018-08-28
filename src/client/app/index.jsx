a05682c51913db010a930f4dfbdb98a7f688dfd8
ericzhu1990 <kaiqiang_zhu@outlook.com>
2018-08-28 01:57:38 -0700

    edit

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Map, Marker, Popup, TileLayer,Rectangle ,LayersControl,FeatureGroup,LayerGroup,Circle} from 'react-leaflet'
 import L from 'leaflet';
+import './index.css';
+import {geolocated} from 'react-geolocated';
+
 const { BaseLayer, Overlay } = LayersControl
 const position = [51.505, -0.09]
 const center = [51.505, -0.09]
@@ -14,60 +17,41 @@ L.Icon.Default.mergeOptions({
 
 
 const map = (
+  <div>
+  <img src={require('../assets/logo.png')} className="index"/>  
   <Map center={position} zoom={13} >
      <LayersControl position="topright">
-          <BaseLayer checked name="OpenStreetMap.Mapnik">
+          <BaseLayer checked name="OpenStreetMap">
             <TileLayer
               attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
           </BaseLayer>
-          <BaseLayer name="OpenStreetMap.BlackAndWhite">
+          <BaseLayer name="BlackAndWhite">
             <TileLayer
               attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
               url="https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
             />
           </BaseLayer>
           <Overlay name="Marker with popup">
-            <Marker position={center}>
-              <Popup>
-                <span>
-                  A pretty CSS3 popup. <br /> Easily customizable.
-                </span>
-              </Popup>
-            </Marker>
+
           </Overlay>
           <Overlay checked name="Layer group with circles">
             <LayerGroup>
-              <Circle center={center} fillColor="blue" radius={200} />
-              <Circle
-                center={center}
-                fillColor="red"
-                radius={100}
-                stroke={false}
-              />
+
               <LayerGroup>
-                <Circle
-                  center={[51.51, -0.08]}
-                  color="green"
-                  fillColor="green"
-                  radius={100}
-                />
+
               </LayerGroup>
             </LayerGroup>
           </Overlay>
           <Overlay name="Feature group">
-            <FeatureGroup color="purple">
-              <Popup>
-                <span>Popup in FeatureGroup</span>
-              </Popup>
-              <Circle center={[51.51, -0.06]} radius={200} />
-              <Rectangle bounds={rectangle} />
-            </FeatureGroup>
+
           </Overlay>
         </LayersControl>
  
   </Map>
+
+</div>  
 )
 
 
